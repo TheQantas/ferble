e795424e801faf89f0cfe5c116f7d8cf06e8d1cd
TheQantas <44556872+TheQantas@users.noreply.github.com>
2023-04-04 23:34:17 -0600

    Update script.js
    Removed typescript exports code

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
 const fuzzysort_1 = __importDefault(require("fuzzysort"));
 let songIndex = -1;
 let totalPointsEarned = 0;
